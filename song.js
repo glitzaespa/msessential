5ac0965a720d7972dc6ba2f74195a4262a61e096
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2023-04-29 23:13:32 -0400

    Create song.js

---
+++ song.js
@@ -0,0 +1,12 @@
+{
+"melanie1": [
+  {
+    "name":"THE CONTORTIONIST",
+    "artist":"Melanie Martinez",
+    "album":"PORTALS",
+    "cover":"https://ismorbo.com/wp-content/uploads/2023/04/portals.jpg",
+    "genre":"pop, alt",
+    "runway":"forest, elf, nymph, creature",
+  }
+  ]
+}
